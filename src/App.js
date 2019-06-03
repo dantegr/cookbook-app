72b0c3d05cb791cc0ce1013108b2c63aa79b0a17
Pantelis <tampantelis@gmail.com>
2019-06-03 14:48:53 +0300

    Added recipe component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './components/Recipe';
 import './App.css';
 
 const App = () => {
@@ -7,10 +7,33 @@ const App = () => {
   // EDAMAM API KEYS
   const APP_ID = '4223c089';
   const APP_KEY = "be0ba7cbcc7558fef43714bc302e8230";
-  
+
+  const [recipes, setRecipes ] = useState([]);
+
+  useEffect(()=> {
+   getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button  className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe 
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image ={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 }
