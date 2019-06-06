227d55a7e204187dd824e7df8f43cf530a3bf18f
Pantelis <tampantelis@gmail.com>
2019-06-06 15:51:09 +0300

    added a spinner while waiting for data to be feteched

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React, {useEffect, useState} from 'react';
 import Recipe from './components/Recipe';
 import Header from './components/Header';
 import './App.css';
+import { css } from '@emotion/core';
+import { HashLoader } from 'react-spinners';
+
 
 const App = () => {
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
 
   // EDAMAM API KEYS
   const APP_ID = '4223c089';
@@ -12,17 +21,22 @@ const App = () => {
   const [recipes, setRecipes ] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState ('');
+  const [isLoading, setIsLoading] = useState(false);
 
 
   useEffect(()=> {
    getRecipes();
   }, [query]);
 
+
+
   const getRecipes = async () => {
+    setIsLoading(true);
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
+    setIsLoading(false);
   }
 
   const updateSearch = e => {
@@ -36,6 +50,8 @@ const App = () => {
     setSearch("");
   };
 
+
+
   return (
     <div className="App">
       <div className="container">
@@ -44,18 +60,30 @@ const App = () => {
           <input className="search-bar" type="text" value={search} onChange={updateSearch} />
           <button className="search-button" type="submit">Search</button>
         </form>
-        <div className="recipes">
-        {recipes.map(recipe => (
-          <Recipe
-            key={recipe.recipe.label} 
-            title={recipe.recipe.label} 
-            calories={recipe.recipe.calories} 
-            image ={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
+        {isLoading ? (
+          <div className='sweet-loading'>
+          <HashLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={'#006400'}
           />
-        ))}
-        </div>
+        </div> 
+        ) : (
+          <div className="recipes">
+          {recipes.map(recipe => (
+            <Recipe
+              key={recipe.recipe.label} 
+              title={recipe.recipe.label} 
+              calories={recipe.recipe.calories} 
+              image ={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+          ))}
+          </div>
+          )}
       </div>
+      
     </div> 
   );
 }
