dc3da61b9fff1711f7ea1d4b7619e1ba0464e4c1
Pantelis <tampantelis@gmail.com>
2019-06-06 12:18:42 +0300

    added heroku deployment build

---
+++ build/precache-manifest.224d043c985adb58759f109899d20be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179cb1355d0490b97ef3f02ce0237204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1c3709ef33c11a83d8",
+    "url": "/static/css/main.ccde20da.chunk.css"
+  },
+  {
+    "revision": "8a60d46b13f7f677b6bd",
+    "url": "/static/js/2.fd6f0f01.chunk.js"
+  },
+  {
+    "revision": "ab1c3709ef33c11a83d8",
+    "url": "/static/js/main.41efbe4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
